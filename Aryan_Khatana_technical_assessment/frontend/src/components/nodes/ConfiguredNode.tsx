b5edb8adfab09fb411840202f0bbda4b6512761d
ARYAN KHATANA <aryankhatana96@gmail.com>
2026-07-10 23:32:00 +0530

    Remove venv, env and zip

--- Aryan_Khatana_technical_assessment/frontend/src/components/nodes/ConfiguredNode.tsx
+++
@@ -1,8 +0,0 @@
-import { BaseNode } from "@/components/nodes/BaseNode";
-import type { NodeConfig, PipelineNodeProps } from "@/types/pipeline";
-
-export function createConfiguredNode(config: NodeConfig) {
-  return function ConfiguredNode(props: PipelineNodeProps) {
-    return <BaseNode {...props} config={config} />;
-  };
-}
